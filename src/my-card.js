81338a75f18adb9144041cf8682620f5b7bea008
Ivory Hodiny <hodiny.ivory@gmail.com>
2024-02-04 19:21:53 -0500

    Added my-card

--- src/my-card.js
+++ src/my-card.js
@@ -1,4 +1,4 @@
-aimport { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from 'lit';
 
 /**
  * Now it's your turn. Here's what we need to try and do
@@ -19,24 +19,65 @@ export class MyCard extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: inline-flex;
+        display:inline-flex;
       }
-      div{
-        background-color:#000080;
-        padding:8px;
-        margin:16px;
-        width: 400px;
+
+      .box{
+        display: flex;
+        flex-wrap: wrap;
+        height: 400px;
+        align-content: space-between;
       }
+      h1{
+	      font-size:25px;
+	      margin-top:8px;
+	      text-align:center;
+        }
+
+      .card-wrapper{
+        height:300px;
+        max-width:180px;
+		    margin:15px;
+		    background:#e5e5e5;
+		    border:1px solid #0d174e;
+		    text-align:center;
+      }
+      .card-img{
+            max-width:180px;
+            max-height:180px;
+				    border-bottom:4px solid #0d174e;
+      }
+      .btn{
+        background-color:#0d174e;
+        color:white;
+      }
+      
     `;
   }
 
   render() {
-    return html`<a href="${this.rlink}"><span>${this.title}</span>`;
+    return html`
+    <div class="box">
+    <div class="card-wrapper">
+      <img class="card-img" src="${this.image}">
+      <h1>${this.title}</h1>
+      <p>${this.description}</p>
+      <div class="btn-wrapper">
+        <a href="${this.link}">
+          <button class="btn">Learn More</button>
+        </a>
+      </div>
+  </div>
+  </div>
+      `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      link: {type: String},
+      image: {type: String},
+      description:{type: String},
     };
   }
 }
