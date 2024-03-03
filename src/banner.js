c179f98a70420a538d26d4571b9944326d5f1333
Ivory Hodiny <hodiny.ivory@gmail.com>
2024-03-02 22:19:30 -0500

    office hours

--- src/banner.js
+++ src/banner.js
@@ -8,12 +8,12 @@ export class BannerApp extends LitElement {
     super();
     this.infotxt="this is info txt. Oh no something really bad has happened! KEEP A LOOK OUT";
     this.status="emergency";
+    this.open=true;
   }
   static get styles() {
     return css`
-        :host {
+      :host {
         --display-mode: true;
-        display: inline;
       }
       :host([status="emergency"]){
         --main-color:#CE2029;
@@ -27,11 +27,26 @@ export class BannerApp extends LitElement {
         --main-color:#3f5277;
       }
       .banner-wrapper{
-        background-color:var(--secondary-color);
+        background-color: var(--secondary-color);
         top:0px;
         position: sticky;
+        z-index: 100;
+        width: 100%;
+        height: 150px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+
 
       }
+
+      .btn-area{
+        height: 100%;
+        width: 15%;
+      }
+
+
       .banner-content{
         margin:0 auto;
         height:100%;
@@ -52,9 +67,6 @@ export class BannerApp extends LitElement {
         background-color:transparent;
         border:0px;
         color:black;
-        position:absolute;
-        top:8px;
-        right:10px;
       }
       .closebtn:hover{
         opacity:1;
@@ -78,45 +90,49 @@ export class BannerApp extends LitElement {
         display:var(--display-title);
       }
       .date{
-        text-align:left;
+        display: flex;
+        flex-wrap: wrap;
   
+        padding: 16px;
       }
       .message-text{
         transform: skew(-20deg);
-        padding-left: 20px;
-        width: 80%;
+        padding-left: 16px;
       }
+
+      .content{
+        height: 100%;
+        width: 70%;
+        transform:skew(20deg);
+        background-color: var(--main-color);
+      }
+
+
+
 
     `;
   }
-
-  render() {
+  openedView(){
     return html`
-    <div class="banner-wrapper">
-      <div class="banner-content">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="alert-icon">
-        <g transform="translate(-350.099 -428.714)"><g transform="translate(350.099 428.714)" fill="none" stroke-width="6">
-          <circle cx="50%" cy="50%" r=30 fill="#F10529"></circle>
-          </g><g transform="translate(395 455)"><rect width="10" height="7" transform="translate(0.919 34.336)">
-          </rect><path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)">
-          </path></g></g></svg>
-        <div class="banner-title">
-          <h1 class="title">TEST ALERT</h1>
+      <div class="banner-wrapper">
+        <div>
+          <p class="date">12th Feburary 2024</p>
         </div>
-          <div class="message-wrapper">
-            <div class="message-text">
-              <slot></slot>
-              <p class="date">12th Feburary 2024</p>
-            </div>
+        <div class="content">
+            <svg class="message-text" style="height: 50px; width 50px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg>
+            <p class="message-text">This is quite possibly the best text that has ever been writtten in the history of ever</p>
         </div>
-        <button class="openbtn" @click="${this.openbtn}">⌄</button>
-
-      <div class="btn-wrapper">
-          <button class="closebtn" @click="${this.closebtn}">&times Close</button>
-      </div>
+        <div class="btn-area">
+          <button class="close">&times Close</button>
+        </div>
     </div>
     `;
   }
+  render() {
+    return (this.open) ? this.openedView() : this.closedView();
+  }
+
+    
  
   static get properties() {
     return {
