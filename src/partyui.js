59ce6b2b92f48b4e321db31e1bc58bfc4af2ebe6
Ivory Hodiny <hodiny.ivory@gmail.com>
2024-03-24 17:53:48 -0400

    PARTYUI

--- src/partyui.js
+++ src/partyui.js
@@ -1,13 +1,15 @@
 import { LitElement, html, css } from 'lit';
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import "@lrnwebcomponents/rpg-character/rpg-character.js";
 
-export class PartyUI extends LitElement {
+export class PartyUI extends DDD{
   
   static get tag() {
     return 'party-ui';
   }
   constructor() {
     super();
+    this.playersarray=[];
   }
   static get styles() {
     return css`
@@ -20,14 +22,19 @@ export class PartyUI extends LitElement {
 
   render() {
     return html`
-    <div>PARTY-UI</div>
+    <div class="partyui">PARTY-UI</div>
+    <rpg-character hat="random" seed="yourchar"></rpg-character>
+   <div class="addbtn">
+      <button>+</button>
+      <p>ADD USER</p>
+  </div>
    
-    `;
+   `;
   }
 
   static get properties() {
     return {
-
+      playersarray:{type:Array},
     };
   }
 }
